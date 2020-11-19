147ecb729e7d9a610efe6721d326509eb1bdb369
adsonatural <adsonhenriquesilva@gmail.com>
2020-11-19 19:18:37 +0100

    Refactoring code for use new adaptative components and responsivity

--- lib/components/transaction_list.dart
+++ lib/components/transaction_list.dart
@@ -10,59 +10,72 @@ class TransactionList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 520,
-      child: transactions.isEmpty ? Column(
-        children: [
-          Text(
-            'Nenhuma transação cadastrada!',
-            style: Theme.of(context).textTheme.headline6,
-          ),
-          SizedBox(height: 40),
-          Container(
-            height: 200,
-            child: Image.asset(
-              'assets/images/waiting.png',
-              fit: BoxFit.cover,  
-            ),
-          ),
-        ],
-      ) : ListView.builder(
-        itemCount: transactions.length,
-        itemBuilder: (ctx, index) {
-          final transaction = transactions[index];
-          return Card(
-            elevation: 3,
-            margin: EdgeInsets.symmetric(
-              vertical: 5,
-              horizontal: 5,
-            ),
-            child: ListTile(
-              leading: CircleAvatar(
-                radius: 30,
-                child: Padding(
-                  padding: const EdgeInsets.all(6),
-                  child: FittedBox(
-                    child: Text('R\$${transaction.value}')
+    return transactions.isEmpty
+        ? LayoutBuilder(
+            builder: (ctx, constraints) {
+              return Column(
+                children: [
+                  SizedBox(height: constraints.maxHeight * 0.05),
+                  Text(
+                    'Nenhuma transação cadastrada!',
+                    style: Theme.of(context).textTheme.headline6,
                   ),
+                  SizedBox(height: constraints.maxHeight * 0.05),
+                  Container(
+                    height: constraints.maxHeight * 0.6,
+                    child: Image.asset(
+                      'assets/images/waiting.png',
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ],
+              );
+            },
+          )
+        : ListView.builder(
+            itemCount: transactions.length,
+            itemBuilder: (ctx, index) {
+              final transaction = transactions[index];
+
+              return Card(
+                elevation: 3,
+                margin: EdgeInsets.symmetric(
+                  vertical: 5,
+                  horizontal: 5,
                 ),
-              ),
-              title: Text(
-                transaction.title,
-                style: Theme.of(context).textTheme.headline6,
-              ),
-              subtitle: Text(
-                DateFormat('d MMM y').format(transaction.time),
-              ),
-              trailing: IconButton(
-                icon: Icon(Icons.delete), 
-                color: Theme.of(context).errorColor,
-                onPressed: () => onRemove(transaction.id)
-              ),
-            ),
+                child: ListTile(
+                  leading: CircleAvatar(
+                    radius: 30,
+                    child: Padding(
+                      padding: const EdgeInsets.all(6),
+                      child: FittedBox(
+                        child:
+                            Text('R\$${transaction.value.toStringAsFixed(2)}'),
+                      ),
+                    ),
+                  ),
+                  title: Text(
+                    transaction.title,
+                    style: Theme.of(context).textTheme.headline6,
+                  ),
+                  subtitle: Text(
+                    DateFormat('d MMM y').format(transaction.time),
+                  ),
+                  trailing: MediaQuery.of(context).size.width > 400
+                      ? FlatButton.icon(
+                          onPressed: () => onRemove(transaction.id),
+                          label: Text('Excluir'),
+                          icon: Icon(Icons.delete),
+                          textColor: Theme.of(context).errorColor,
+                        )
+                      : IconButton(
+                          icon: Icon(Icons.delete),
+                          color: Theme.of(context).errorColor,
+                          onPressed: () => onRemove(transaction.id),
+                        ),
+                ),
+              );
+            },
           );
-        },
-      ),
-    );
   }
 }
